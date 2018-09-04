614593a3afa935b41a2c61523872cc832b19a62e
Mark Spencer <Marksman460@gmail.com>
2018-09-03 18:31:49 -0700

    Created an actual website
    Something more I can be proud of.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Smoot Scrolling
+$('#view-work').on('click', function() {
+  const images = $('#images').position().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
